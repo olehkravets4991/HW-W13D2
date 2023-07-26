d96aa2dbd213c41d0f5bb78dfd3c07d0ed0de652
Oleh <kravetsoleh4991@gmail.com>
2023-07-26 00:38:30 -0700

    commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,6 +3,7 @@ import Movie from "./Movie"
 function Movies(props){
     return(<div>
             <h1> Marvel Iron Man Movies</h1>
+            <h2>“I am Iron Man”- Tony Stark</h2>
             <div className="container">
                 {props.movies.map((item, index)=>{
                 return (
